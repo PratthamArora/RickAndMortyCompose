011caea528dbfb6b45f1dee0415773aa788c29e4
PratthamArora <arora.prattham@gmail.com>
2021-06-13 21:44:32 +0530

    setup dependencies and base nav controller

---
+++ app/src/main/java/com/pratthamarora/rickandmortycompose/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.pratthamarora.rickandmortycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.pratthamarora.rickandmortycompose.ui.theme.RickAndMortyComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyComposeTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "character_list_screen"
+                ) {
+                    composable("character_list_screen") {
+
+                    }
+                    composable("character_detail_screen/{dominantColor}/{characterId}",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("characterId") {
+                                type = NavType.IntType
+                            }
+                        )) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let {
+                                Color(it)
+                            } ?: Color.White
+                        }
+                        val characterId = remember {
+                            it.arguments?.getInt("characterId")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
